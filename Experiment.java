35f7a02da2caea9311eea12e0b028abdf96d36db
k11logram <161375355+k11logram@users.noreply.github.com>
2024-03-21 16:24:51 +0200

    Add files via upload

--- Experiment.java
+++ Experiment.java
@@ -5,22 +5,31 @@
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JOptionPane;
+
+
 
 
 public class Experiment{
  
   public static void main(String args[]){
-    Scanner keyboard = new Scanner(System.in);
-    String input="";
+    JFrame frame = new JFrame("Experimental Data");
+    
+    frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().setBackground(Color.BLACK);
+    frame.setVisible(true);
+    String input = JOptionPane.showInputDialog(frame,"Please enter the number of generic truths you wish to store in the tree, must be from 1 to 50000");
+    
     int x=0;
     List<Integer> insertCounter = new ArrayList<>();
     List<Integer> searchCounter = new ArrayList<>();
-    System.out.println("Please enter the number of generic truths you wish to store in the tree, must be from 1 to 50000");
-    input= keyboard.nextLine().trim();
       do{
       
         //Variable declaration
@@ -32,64 +41,62 @@ public class Experiment{
         List<Fact> nodeStorage = new ArrayList<>();
        
 
-        if(!(input.equals("Stop")||input.equals(""))){
-           int Numterms=Integer.parseInt(input);
-        //Store the query terms in a array
-          try{
-          BufferedReader read = new BufferedReader(new FileReader("GenericsKB-queries.txt"));
-          String fileLine="";
-      
-        
-          while((fileLine=read.readLine())!= null){
-            queries[i]=fileLine;
-            i++;
-            }
-          read.close();
-          
-          }
-          catch(IOException e){
-          System.out.println("Enter proper file name");
-            } 
-       
-
-
-       // Storing the generic truths in a AVL tree 
+        int Numterms=Integer.parseInt(input);
+    //Store the query terms in a array
         try{
-        BufferedReader readfile = new BufferedReader(new FileReader("GenericsKB.txt"));
-        String file ="";
-        while((file = readfile.readLine()) != null){
-          String term = file.substring(0, file.indexOf("\t")+1);
-          String no_item=file.substring(file.indexOf("\t")+1, file.length());
-          String statement = no_item.substring(0,no_item.indexOf("\t")+1);
-          double confidence = Double.parseDouble(no_item.substring(no_item.indexOf("\t")+1,no_item.length()));
-          
+        BufferedReader read = new BufferedReader(new FileReader("GenericsKB-queries.txt"));
+        String fileLine="";
+   
+    
+        while((fileLine=read.readLine())!= null){
+          queries[i]=fileLine;
+          i++;
+          }
+        read.close();
         
-          Fact truth = new Fact(term,statement,confidence);
-          nodeStorage.add(truth);
-          
-          }
-        readfile.close();
-        Collections.shuffle(nodeStorage);
-          }
-          
-      catch (IOException e){
-            System.out.println("Enter proper file name");
-      
-       }
+        }
+        catch(IOException e){
+        System.out.println("Enter proper file name");
+          } 
+    
 
-      while(j<Numterms){
-        node.insert(nodeStorage.get(j));
-        j++;
-      }
-       //Comparing the terms in the query file to the terms in the AVL tree
-      for(int y=0;y<i;y++){   
-        AVLNode newNode=node.find(queries[y]);
-        if(newNode != null){
-                 //  System.out.println(queries[y]+": "+newNode.data.getStatement().trim()+" ("+newNode.data.getConfidence()+")");
-          }
-        else{}
-      }
-      }
+
+    // Storing the generic truths in a AVL tree 
+    try{
+    BufferedReader readfile = new BufferedReader(new FileReader("GenericsKB.txt"));
+    String file ="";
+    while((file = readfile.readLine()) != null){
+        String term = file.substring(0, file.indexOf("\t")+1);
+        String no_item=file.substring(file.indexOf("\t")+1, file.length());
+        String statement = no_item.substring(0,no_item.indexOf("\t")+1);
+        double confidence = Double.parseDouble(no_item.substring(no_item.indexOf("\t")+1,no_item.length()));
+        
+    
+        Fact truth = new Fact(term,statement,confidence);
+        nodeStorage.add(truth);
+        
+        }
+    readfile.close();
+    Collections.shuffle(nodeStorage);
+        }
+        
+   catch (IOException e){
+          System.out.println("Enter proper file name");
+   
+    }
+
+   while(j<Numterms){
+    node.insert(nodeStorage.get(j));
+    j++;
+   }
+    //Comparing the terms in the query file to the terms in the AVL tree
+   for(int y=0;y<i;y++){   
+    AVLNode newNode=node.find(queries[y]);
+    if(newNode != null){
+               //  System.out.println(queries[y]+": "+newNode.data.getStatement().trim()+" ("+newNode.data.getConfidence()+")");
+        }
+    else{}
+   }
     insertCounter.add(node.getInsertCount());
     searchCounter.add(node.getSearchCount());
 
@@ -98,8 +105,9 @@ public class Experiment{
    }
    
   while((x<10));
-  System.out.println(insertCounter);
-  System.out.println(searchCounter);
+ JOptionPane.showMessageDialog(frame,"insertMethod Comparisons"+insertCounter); 
+ JOptionPane.showMessageDialog(frame,"SerachMethodComparisons"+searchCounter);
+ frame.dispose();
 }
 }
     
